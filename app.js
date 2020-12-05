72182bb87d87a3a56c3308fa8a0dbdde21752694
dominicdaviescodes <codewithdom@gmail.com>
2020-12-05 18:24:38 +0000

    use map function to map the number load with opacity value and the background with opacity value

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.br');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+// run every 30ms
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  // increment every 30ms
+  load++;
+  // upto 100
+  if (load > 99) {
+    // stop at 100
+    clearInterval(int);
+  }
+  // change the text/ numbers 0 - 100%
+  loadText.innerText = `${load}%`;
+  // start at full opacity (1) to (0).  need to map 0-100 (the %) to 1-0 (opacity)
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  // 30px is the max blur. so we map 0-100 with 30 - 0 (max blur to no blur)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
